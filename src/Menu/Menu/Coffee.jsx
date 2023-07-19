6ad354f70e30328b7e18873ad01b2bc43a94a9c3
gxxu0 <callsim01@naver.com>
2023-07-19 13:13:19 +0900

    v menu cart

--- src/Menu/Menu/Coffee.jsx
+++ src/Menu/Menu/Coffee.jsx
@@ -1,78 +1,101 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import Cart from "./Cart";
-// import Modal from "./Modal"
-import './Style.css'
-
+import swal from "sweetalert";
+import "./Style.css";
 
 export default function Coffee(props) {
-
   const [cartItems, setCartItems] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMenu, setSelectedMenu] = useState(null);
 
   const addToCart = (product) => {
     setCartItems([...cartItems, product]);
+    swal({
+      title: "Success",
+      text: "장바구니에 추가되었습니다!",
+      icon: "success",
+      buttons: false,
+      timer: 1500,
+    });
+    setTimeout(() => {
+      swal.close();
+    }, 1000);
   };
 
   const removeFromCart = (product) => {
     const updatedCartItems = cartItems.filter((item) => item.id !== product.id);
     setCartItems(updatedCartItems);
-  };//cart에 목록 추가, 삭제
+  };
 
-  const [isModalOpen, setIsModalOpen] = useState('modalId');
-
-  const openModal = (modalId) => {
-    setIsModalOpen(modalId);
+  const openModal = (coffeeInfo) => {
+    setSelectedMenu(coffeeInfo);
+    setIsModalOpen(true);
+     
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
+  const addToFavorites = (coffeeInfo) => {
+    swal("Success", "Added to Favorites!", "success");
+  };
+
   return (
-
     <>
-    <div>
-      <div className="Menu-b_div">
-        {props.coffeeInfo.map( (coffeeInfo, index) => (
-          <div>
-            <button
-              onClick={() => openModal(index)}
-              type="button"
-              className="Menu-btnMenu_Body"
-              id="btn_b3"
-            >
-              <img src={`data:imge/jpeg;base64,${coffeeInfo.imageBytes}`} className="img" alt={coffeeInfo.name} />
-            </button>
-
-            {isModalOpen === index && (
-            <div className="Menu-containerMenu">
-              <img src={`data:image/jpeg;base64,${coffeeInfo.imageBytes}`} className="img" alt={coffeeInfo.name} />
-              <p className="Menu-mini_title">{coffeeInfo.name}</p>
-              <p><text className="Menu-text1">{coffeeInfo.content}</text></p>
-              <button className="Menu-btnMenu" onClick={closeModal}>닫기</button>
-              <button className="Menu-btnMenu" onClick={() => 
-                Cart.addToCart(
-                  { 
-                    id: 'btn_c1', 
-                    name: `${coffeeInfo.name}`, 
-                    price: `${coffeeInfo.price}` 
-                    })}>
-                      Keep
+      <div className="Menu-b_div-wrapper">
+        <div className="Menu-b_div">
+          {props.coffeeInfo.map((coffeeInfo, index) => (
+            <div key={index}>
+              <button
+                onClick={() => openModal(coffeeInfo)}
+                type="button"
+                className="Menu-btnMenu_Body"
+                id="btn_b3"
+              >
+                <img
+                  src={`data:imge/jpeg;base64,${coffeeInfo.imageBytes}`}
+                  className="img"
+                  alt={coffeeInfo.name}
+                />
               </button>
+              <p className="Menu-btnMenu_menuName">{coffeeInfo.name}</p>
+
+              {isModalOpen && selectedMenu && selectedMenu === coffeeInfo && (
+                <div className="Menu-containerMenu">
+                  <div>
+                    <button className="Menu-btnMenu-close" onClick={closeModal}>
+                      X
+                    </button>
+                  </div>
+                  <img
+                    src={`data:image/jpeg;base64,${coffeeInfo.imageBytes}`}
+                    className="img"
+                    alt={coffeeInfo.name}
+                  />
+                  <p className="Menu-mini_title">{coffeeInfo.name}</p>
+                  <p>
+                    <text className="Menu-text1">{coffeeInfo.content}</text>
+                  </p>
+                  <button
+                    className="Menu-btnMenu"
+                    onClick={() => addToFavorites(coffeeInfo)}
+                  >
+                    즐겨찾기
+                  </button>
+                  <button
+                    className="Menu-btnMenu"
+                    onClick={() => addToCart(coffeeInfo)}
+                  >
+                    장바구니
+                  </button>
+                </div>
+              )}
             </div>
-          )}
-          </div>
-        ))
-        }
+          ))}
+        </div>
       </div>
-    </div>
-      
-      
-      {/* <div>
-        <Cart cartItems={cartItems} addToCart={addToCart} removeFromCart={removeFromCart} />
-      </div> */}
+      <Cart cartItems={cartItems} removeFromCart={removeFromCart} />
     </>
-  )
+  );
 }
-
-
